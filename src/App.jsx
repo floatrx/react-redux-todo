e0e2ba6699078be9ad34e2e978e5aa924645d65d
ifloatrx <ifloatrx@gmail.com>
2020-01-04 15:46:12 +0200

    Add notifications

--- src/App.jsx
+++ src/App.jsx
@@ -8,107 +8,110 @@ import Header from './components/Header';
 import Todo from './components/Todo';
 import Form from './components/Form';
 import Stats from './components/Stats';
+import { notify } from './components/Notifications';
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            todos: [],
-            editMode: false
-        };
-    }
-
-    componentDidMount = () => {
-        axios
-            .get('/api/todos')
-            .then(response => {
-                let todos = response.data;
-                this.setState({ todos });
-            })
-            .catch(this.handleError);
+    this.state = {
+      todos: [],
+      editMode: false,
     };
+  }
 
-    handleToggle = id => {
-        axios
-            .patch(`/api/todos/${id}`)
-            .then(response => {
-                let todos = this.state.todos.map(todo => {
-                    if (todo.id === id) {
-                        todo.completed = response.data.completed;
-                    }
-                    return todo;
-                });
-                this.setState({ todos });
-            })
-            .catch(this.handleError);
-    };
+  componentDidMount = () => {
+    axios
+      .get('/api/todos')
+      .then((response) => {
+        let todos = response.data;
+        this.setState({ todos });
+      })
+      .catch(this.handleError);
+  };
 
-    handleDelete = id => {
-        axios
-            .delete(`/api/todos/${id}`)
-            .then(() => {
-                let todos = this.state.todos.filter(todo => todo.id !== id);
-                this.setState({ todos });
-            })
-            .catch(this.handleError);
-    };
+  handleToggle = (id) => {
+    axios
+      .patch(`/api/todos/${id}`)
+      .then((response) => {
+        let todos = this.state.todos.map((todo) => {
+          if (todo.id === id) {
+            todo.completed = response.data.completed;
+          }
+          return todo;
+        });
+        this.setState({ todos });
+      })
+      .catch(this.handleError);
+  };
 
-    hadnleAdd = title => {
-        axios
-            .post('/api/todos', { title })
-            .then(response => {
-                let todo = response.data;
-                let todos = [...this.state.todos, todo];
-                this.setState({ todos });
-            })
-            .catch(this.handleError);
-    };
+  handleDelete = (id) => {
+    axios
+      .delete(`/api/todos/${id}`)
+      .then(() => {
+        let todos = this.state.todos.filter((todo) => todo.id !== id);
+        this.setState({ todos });
+      })
+      .catch(this.handleError);
+  };
 
-    handleEdit = (id, title) => {
-        axios
-            .put(`/api/todos/${id}`, { title })
-            .then(response => {
-                console.log('response', response)
-                let todos = this.state.todos.map(todo => {
-                    if (todo.id === id) {
-                        todo.title = response.data.title;
-                    }
-                    return todo;
-                });
-                this.setState({ todos });
-            })
-            .catch(this.handleError);
-    };
+  hadnleAdd = (title) => {
+    axios
+      .post('/api/todos', { title })
+      .then((response) => {
+        let todo = response.data;
+        let todos = [...this.state.todos, todo];
+        this.setState({ todos });
+      })
+      .catch(this.handleError);
+  };
 
-    handleError = error => {
-        console.error(error.message);
-    };
+  handleEdit = (id, title) => {
+    axios
+      .put(`/api/todos/${id}`, { title })
+      .then((response) => {
+        console.log('response', response);
+        let todos = this.state.todos.map((todo) => {
+          if (todo.id === id) {
+            todo.title = response.data.title;
+          }
+          return todo;
+        });
+        this.setState({ todos });
+      })
+      .catch(this.handleError);
+  };
 
-    render() {
-        return (
-            <main className="App todo">
-                <Header title={this.props.title} todos={this.state.todos} />
-                <Stats todos={this.state.todos} />
-                <section className="todo__list">
-                    {this.state.todos.map(todo => (
-                        <Todo
-                            key={todo.id}
-                            id={todo.id}
-                            title={todo.title}
-                            completed={todo.completed}
-                            onChange={this.handleToggle}
-                            onDelete={this.handleDelete}
-                            onEdit={this.handleEdit}
-                        />
-                    ))}
-                </section>
-                <Form onAdd={this.hadnleAdd} />
-            </main>
-        );
-    }
+  handleError = ({message}) => {
+    console.error('Error handled!!!!', message);
+    // console.log('BAD1')
+    notify.show({ message });
+  };
+
+  render() {
+    return (
+      <main className="App todo">
+        <Header title={this.props.title} todos={this.state.todos} />
+        <Stats todos={this.state.todos} />
+        <section className="todo__list">
+          {this.state.todos.map((todo) => (
+            <Todo
+              key={todo.id}
+              id={todo.id}
+              title={todo.title}
+              completed={todo.completed}
+              onChange={this.handleToggle}
+              onDelete={this.handleDelete}
+              onEdit={this.handleEdit}
+            />
+          ))}
+        </section>
+        <Form onAdd={this.hadnleAdd} />
+      </main>
+    );
+  }
 }
 
 App.propTypes = {
-    title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
 };
---
+++ src/components/Notifications.jsx
@@ -0,0 +1,74 @@
+// Documentation of "react-notifications-component"
+// Here: https://www.npmjs.com/package/react-notifications-component
+
+import ReactNotification, { store } from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css';
+
+const defaults = {
+  title: '',
+  message: '',
+  type: 'info',
+  insert: 'bottom',
+  container: 'top-right',
+  // animationIn: ['notifyShow'],
+  // animationOut: ['notifyHide'],
+  width: 320,
+  dismiss: {
+    duration: 3000,
+    onScreen: true,
+    pauseOnHover: true,
+  },
+  touchSlidingExit: {
+    swipe: {
+      duration: 1000,
+      timingFunction: 'ease-out',
+      delay: 0,
+    },
+    fade: {
+      duration: 1000,
+      timingFunction: 'ease-out',
+      delay: 0,
+    },
+  },
+};
+
+const notify = (function() {
+
+  const show = (options) => {
+    store.addNotification({
+      ...defaults,
+      ...options,
+    });
+  };
+
+  const success = (options) => {
+    store.addNotification({
+      ...defaults,
+      ...options,
+      type: 'success',
+    });
+  };
+  const error = (options) => {
+    store.addNotification({
+      ...defaults,
+      ...options,
+      type: 'danger',
+    });
+  };
+  const warning = (options) => {
+    store.addNotification({
+      ...defaults,
+      ...options,
+      type: 'warning',
+    });
+  };
+
+  return {
+    show,
+    success,
+    error,
+    warning,
+  };
+})();
+
+export { ReactNotification as Notification, notify };
